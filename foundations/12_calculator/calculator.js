630b19670888489d464280bd435429f06453801c
AMN-47 <akhilnagar57@gmail.com>
2026-01-27 15:20:01 +0200

    Added to functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,21 +1,21 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num1, num2) {
+	return num1 + num2;
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  return num1 * num2;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ^ num2;
 };
 
 const factorial = function() {
