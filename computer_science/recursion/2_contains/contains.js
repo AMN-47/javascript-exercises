c42255e272a8fe689290653052e68971f7b875c2
AMN-47 <akhilnagar57@gmail.com>
2026-03-23 15:11:20 +0200

    Added contains function and all tests pass

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -2,6 +2,14 @@ const contains = function(object, searchValue) {
     const values = Object.values(object)
   
     if (values.include(searchValue)) return true;
+
+    const nestedObjects = values.filter(
+        (value) => typeof value === "object" && value !== null
+    );
+
+    return nestedObjects.some((nestedObject) =>
+    contains(nestedObject, searchValue)
+  );
 };
   
 // Do not edit below this line
