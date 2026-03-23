e486ca1378e3a6d94b0ed9d4eba43e704f2ce5ee
AMN-47 <akhilnagar57@gmail.com>
2026-03-23 16:29:16 +0200

    Added base case for empty array

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,8 @@
-const permutations = function() {
-  
+const permutations = function(nums) {
+    //1. Base Case = if Array is empty
+    if (nums.length === 0) {
+        return[[]];
+    }
 };
   
 // Do not edit below this line
