6ae758b8667e3b9e28f17db34f3745c1870006cb
AMN-47 <akhilnagar57@gmail.com>
2026-03-23 16:25:01 +0200

    Added forloop and if statement to iterate until test passes

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,23 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(data) {
+  //1. validate if input is array or plain object
+  if (typeof data !== 'object' || data === null || typeof data === 'function') {
+    return undefined;
+  }
+
+  let count = 0;
+
+  //2. iterate through collection of data
+  for (let value of Object.values(data)) {
+    //3. check if current number is an integr
+    if (Number.isInteger(value)) {
+        count++;
+    }
+    //4. Recursive steup
+    else if (typeof value === 'object' && value !== null) {
+        count += totalIntegers(value);
+    }
+  }
+  return count
 };
   
 // Do not edit below this line
