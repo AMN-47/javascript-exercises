d80c57aec68bc118f4e08a2936005d9abf107d62
AMN-47 <akhilnagar57@gmail.com>
2026-03-23 15:08:05 +0200

    if statement for search value

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,7 @@
-const contains = function() {
+const contains = function(object, searchValue) {
+    const values = Object.values(object)
   
+    if (values.include(searchValue)) return true;
 };
   
 // Do not edit below this line
