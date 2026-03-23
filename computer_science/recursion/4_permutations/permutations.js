1238c0029a342051d64a28fb705c41d31dd0b24f
AMN-47 <akhilnagar57@gmail.com>
2026-03-23 19:13:44 +0200

    Added code to run through permutations, all tests passed

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -3,6 +3,24 @@ const permutations = function(nums) {
     if (nums.length === 0) {
         return[[]];
     }
+
+    const result = [];
+    //2. Iterate through each number in the array to treat it as the first element
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+
+        //3. Ceate sub array for all numbers excepr for the currnr 1
+        const remainingNums = nums.filter((_, index) => index !== i);
+
+        //4. Recursive step: Get all permutations of the remaining numbers.
+        const innerPermutations = permutations(remainingNums);
+
+        //combine and atach the current number to the front of the inner permutation
+        for (let subPermutation of innerPermutations) {
+            result.push([currentNum,...subPermutation]);
+        }
+    }
+    return result;
 };
   
 // Do not edit below this line
